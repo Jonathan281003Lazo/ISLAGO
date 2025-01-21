97b197393901b7e31fe6d848abd824b76f8ef003
Jonathan281003Lazo <jonathanjorgemora@gmail.com>
2025-01-20 23:35:12 -0600

    Avance del dia 20/01/2025. Agregamos mas diseños, modificamos la capa datos. Implementaciones e interfaces genericas casi completadas.

---
+++ ISLAGO.Datos/Implementacion/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ISLAGO.Datos.DBContext;
+using ISLAGO.Datos.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ISLAGO.Datos.Implementacion
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public GenericRepository()
+        {
+            
+        }
+
+        public Task<TEntity> Obtener(Expression<Func<TEntity, bool>> filtro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> Crear(TEntity entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Editar(TEntity entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Eliminar(TEntity entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<TEntity>> Consultar(Expression<Func<TEntity, bool>> filtro = null)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
